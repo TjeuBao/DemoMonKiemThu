98ed42417d3c5f6213065a980e487eac35400e9a
vanbao <1451010013bao@ou.edu.vn>
2018-01-25 08:33:04 +0700

    them khung dang nhap

--- DemoGithub/DemoGithub/Form1.cs
+++ DemoGithub/DemoGithub/Form1.cs
@@ -15,5 +15,15 @@ namespace DemoGithub
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
